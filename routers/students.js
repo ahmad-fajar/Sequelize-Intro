4f03561b3b54cd1979e07b75f3e1d5e7ccecf256
ahmad-fajar <shin.fajar@gmail.com>
2017-07-13 20:15:36 +0700

    sampai cRUd

--- routers/students.js
+++ routers/students.js
@@ -11,4 +11,39 @@ router.get('/', (req, res) => {
 });
 
 
+// edit
+router.get('/edit/:id', (req, res) => {
+  model.Student.findAll({
+    where : {
+      id : req.params.id
+    }
+  })
+  .then(data => {
+    // console.log(data);
+    res.render('editstudent', {data : data});
+  });
+});
+
+router.post('/edit/:id', (req, res) => {
+  model.Student.update({
+    first_name : `${req.body.first_name}`,
+    last_name : `${req.body.last_name}`,
+    email : `${req.body.email}`
+  }, {
+    where: {
+      id : `${req.params.id}`
+    }
+  })
+  .then(() => {
+    res.redirect('/students');
+  });
+});
+
+
+// add
+router.get('/addstudent', (req, res) => {
+  res.render('addstudent');
+});
+
+
 module.exports = router;
